429a2c27bebcf3b4fe5c6018b80bd9e95ed13614
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-01-30 14:15:13 +0530

    typo

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDatabase = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log("MongoDB Connection Successfull!");
+    console.log("MongoDB Connection Successful!");
   } catch (error) {
     console.log(`MongoDB Connection Failed:${error}`);
     process.exit(1);
